21bb349d6876b2dda40685f25ed2c09449384da5
Diogo Brandon <d_bb2001@hotmail.com>
2022-11-19 22:07:10 -0300

    Finishing UI

--- Criptografia.py
+++ Criptografia.py
@@ -2,76 +2,88 @@ from tkinter import *
 
 alfabeto = "abcdefghijklmnopqrstuvwxyz"
 tamanhoAlfabeto = range(len(alfabeto))
-continuarCripto = "s"
 
 letraIndex = dict(zip(alfabeto, tamanhoAlfabeto))
 posicaoIndex = dict(zip(tamanhoAlfabeto, alfabeto))
 
-def cesarCripto(plaintext, troca):
-    cifra = ''
+def cesarCripto():
+  plaintext = originalText.get().lower()
+  plaintext.lower()
+  troca = int(shift.get())
+  cifra = ""
+  if len(originalText.get()) <= 128:
     for letra in plaintext:
       if letra in alfabeto:
         index = ((letraIndex[letra] + troca) % len(alfabeto))
         letraCifra = posicaoIndex[index]
         cifra += letraCifra
       else:
-        cifra += letra   
-    return cifra 
+        cifra += letra
+    else:
+      result["text"] = "This text is too long. Fit to 128 characters."
+  result["text"] = cifra
 
-def coletaDados():
-  resposta = input("Você deseja Criptografar ou Descriptografar uma mensagem? c/d ")
-  respostaFormat = resposta.lower()
-  if respostaFormat != "d" and respostaFormat != "c":
-    print("Entrada inválida! Tente novamente!")
-  else:
-      mensagem = input("Insira a mensagem: ")
-      if len(mensagem) <= 128:
-        mensagemFormat = mensagem.lower()
-        rotacao = input("Insira a chave numérica: ")
-        numero = rotacao.isnumeric()
-        if numero:
-          rotacao = int(rotacao)
-          if "d" in respostaFormat:
-            print("Descriptografia: " + cesarCripto(mensagemFormat, -rotacao))
-          else:  
-            print("Criptografia: " + cesarCripto(mensagemFormat, rotacao))
-        else:
-<<<<<<< HEAD
-          print("A chave precisa ser um número natural!")
-=======
-          print("A chave precisa ser um número real!")
->>>>>>> 7f85d693d5e60a6d5e444db0d1f5df2b9a24b286
+def caesarDecipher():
+  plaintext = originalText.get().lower()
+  troca = int(shift.get())
+  cifra = ""
+  if len(originalText.get()) <= 128:
+    try:
+      int(shift.get())
+    except ValueError:
+      result["text"] = "Shift needs to be a number."
+    for letra in plaintext:
+      if letra in alfabeto:
+        index = ((letraIndex[letra] - troca) % len(alfabeto))
+        letraCifra = posicaoIndex[index]
+        cifra += letraCifra
       else:
-          print("A mensagem ultrapassa 128 caractéres, tente novamente!")
-
-'''while continuarCripto == "s":
-  coletaDados()
-  continuarCripto = input("Digite a letra 's' para continuar ou qualquer outra tecla para encerrar a execução: ")
-print("Programa finalizado!")'''
+        cifra += letra
+  else:
+    result["text"] = "This text is too long. Fit to 128 characters."
+  result["text"] = cifra
 
 window = Tk()
 window.title("Caesar's Cipher")
 
-textPhrase = Label(window, text = "Enter the text: ")
-textPhrase.grid(column=0, row=0, padx=10, pady=10)
-plaintext = Entry(window, width=50)
-plaintext.grid(column=0, row=1, padx=10, pady=3)
+htuText0 = Label(window, text = "Read some warnings before using:")
+htuText0.grid(column=0, row=0, padx=10, pady=10)
+htuText1 = Label(window, text = "* All uppercase characters will be converted in lowercase;")
+htuText1.grid(column=0, row=1, padx=10)
+htuText2 = Label(window, text = "* All symbols or numbers will remain in the final cipher;")
+htuText2.grid(column=0, row=2, padx=10)
+htuText3 = Label(window, text = "* Character limit is set to in 128;")
+htuText3.grid(column=0, row=3, padx=10)
+htuText4 = Label(window, text = "* Shift must be a natural number;")
+htuText4.grid(column=0, row=4, padx=10)
+htuText5 = Label(window, text = "* All text fields must be filled.")
+htuText5.grid(column=0, row=5, padx=10)
 
-textShift = Label(window, text="Enter the number of shifts: ")
-textShift.grid(column=0, row=2, padx=10, pady=10)
+textPhrase = Label(window, text="Enter the text:")
+textPhrase.grid(column=0, row=6, padx=10, pady=10)
+originalText = Entry(window, width=50)
+originalText.grid(column=0, row=7, padx=10, pady=5)
+
+textShift = Label(window, text="Enter the number of shifts:")
+textShift.grid(column=0, row=8, padx=10, pady=10)
 shift = Entry(window, width=10)
-shift.grid(column=0, row=3, padx=10, pady=3)
+shift.grid(column=0, row=9, padx=10, pady=3)
 
 buttonFrame = Frame(window)
-buttonFrame.grid(column=0, row=4, padx=10, pady=10)
+buttonFrame.grid(column=0, row=10, padx=10, pady=10)
 
-buttonCipher = Button(buttonFrame, text="Cipher text", width=10)
+buttonCipher = Button(buttonFrame, text="Cipher text", width=10, command=cesarCripto)
 buttonCipher.pack(side="left")
 
-buttonDecipher = Button(buttonFrame, text="Decipher text", width=10)
+buttonDecipher = Button(buttonFrame, text="Decipher text", width=10, command=caesarDecipher)
 buttonDecipher.pack(side="right")
 
-Result = Label(window, text="Result here!")
-Result.grid(column=0, row=5, padx=10, pady=10)
+resultText = Label(window, text="Cipher's result:")
+resultText.grid(column=0, row=11, padx=10)
+result = Label(window, text="Waiting...")
+result.grid(column=0, row=12, padx=10)
+
+emptySpace = Label(window, text="")
+emptySpace.grid(column=0, row=13, padx=10, pady=10)
 
 window.mainloop()
