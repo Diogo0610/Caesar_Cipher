f76df3c96c70cbbc85b25e9318fcc82b700b6a7b
Diogo Bonilha Brandon <70177817+Diogo0610@users.noreply.github.com>
2022-11-21 11:59:50 -0300

    Update Caesar_Cipher.py

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
+#variáveis 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabetSize = range(len(alphabet))
 
+#Atribuição de chaves e valores entre as variáveis
 letterIndex = dict(zip(alphabet, alphabetSize))
 positionIndex = dict(zip(alphabetSize, alphabet))
 
@@ -40,9 +42,12 @@ def caesarDecipher():
       result["text"] = "This text is too long. Fit to 128 characters."
   result["text"] = cipher
 
+#Interface
+#Criação da janela
 window = Tk()
 window.title("Caesar's Cipher")
 
+#Textos de orientação
 htuText0 = Label(window, text = "Read some warnings before using:")
 htuText0.grid(column=0, row=0, padx=10, pady=10)
 htuText1 = Label(window, text = "* All uppercase characters will be converted in lowercase;")
@@ -56,16 +61,19 @@ htuText4.grid(column=0, row=4, padx=10)
 htuText5 = Label(window, text = "* All text fields must be filled.")
 htuText5.grid(column=0, row=5, padx=10)
 
+#Coleta do texto inserido no campo
 textPhrase = Label(window, text="Enter the text:")
 textPhrase.grid(column=0, row=6, padx=10, pady=10)
 originalText = Entry(window, width=60)
 originalText.grid(column=0, row=7, padx=10, pady=5)
 
+#Coleta do número inserido no campo
 textShift = Label(window, text="Enter the number of shifts:")
 textShift.grid(column=0, row=8, padx=10, pady=10)
 shift = Entry(window, width=10)
 shift.grid(column=0, row=9, padx=10, pady=3)
 
+#Criação de um frame para armazenar os dois botões
 buttonFrame = Frame(window)
 buttonFrame.grid(column=0, row=10, padx=10, pady=10)
 
@@ -75,6 +83,7 @@ buttonCipher.pack(side="left")
 buttonDecipher = Button(buttonFrame, text="Decipher text", width=10, command=caesarDecipher)
 buttonDecipher.pack(side="right")
 
+#Exibe o resultado do programa
 resultText = Label(window, text="Cipher's result:")
 resultText.grid(column=0, row=11, padx=10)
 result = Label(window, text="Waiting...")
@@ -83,4 +92,5 @@ result.grid(column=0, row=12, padx=10)
 emptySpace = Label(window, text="")
 emptySpace.grid(column=0, row=13, padx=10, pady=10)
 
+#Exibe a janela na tela
 window.mainloop()
